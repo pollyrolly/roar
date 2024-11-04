281867e1b3af77bff3910264148b9bf5007aaa32
Tomic Dana <danakath@amazon.com>
2024-11-04 03:40:03 -0800

    gitclone testing 12

--- update.sh
+++ update.sh
@@ -1,2 +1,18 @@
-#!bin/sh 
-aws cloudformation update-stack --stack-name reignite --template-body file:///Volumes/workplace/Projects/transfer/reignite24-autoredshift-review/templates/Reignite24arr.yaml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM 
+#!/bin/bash 
+if [ -f update ]
+then
+    update=$(cat update)
+else
+    update=0
+fi
+#if update is 0 set update to 1
+if [ $update == 0 ]
+then
+    update=1
+else
+    update=0
+fi
+
+#write update to file update
+echo $update > update
+aws cloudformation update-stack --stack-name reignite --template-body file:///Volumes/workplace/Projects/transfer/reignite24-autoredshift-review/templates/Reignite24arr.yaml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter ParameterKey=Update,ParameterValue=$update
