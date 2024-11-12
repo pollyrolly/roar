55095b7e367db5b88dc18f6840578ab037d8a32b
Tomic Dana <danakath@amazon.com>
2024-11-12 06:56:10 -0800

    removing

--- scripts/delete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-aws cloudformation delete-stack --stack-name reignite
-aws cloudformation wait stack-delete-complete --stack-name reignite
-# delete files trigger and update
-rm -f update
-rm -f trigger
